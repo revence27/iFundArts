f7ca625ad9a19803abe58d2cf0e280c89da8180c
Revence Kalibwani <revence@1st.ug>
2011-11-05 10:09:12 +0300

    Let's get this Party started.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IfundartsPh::Application.config.secret_token = '999b185b689bf46a7ec4ddeb4ac423d443ee7ef4898798ad9393c0e342c8ef4e53bdba2555a49175edc85d63f6902388b03c01b72d7b87201015196238222c80'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+IfundartsPh::Application.config.session_store :cookie_store, :key => '_ifundarts-ph_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# IfundartsPh::Application.config.session_store :active_record_store
