63d69b1bb0ee075856057b2b81d0b5d73fe34506
Revence Kalibwani <revence@1st.ug>
2012-05-05 22:44:00 +0200

    Started pushing some of Faisal’s requirements.

--- app/controllers/ifund_controller.rb
+++ app/controllers/ifund_controller.rb
@@ -2,4 +2,12 @@ class IfundController < ApplicationController
   def index
     @projects = Project.order('ends_at ASC')
   end
+
+  def explore
+    @projects = Project.order('ends_at ASC')
+  end
+
+  def creator
+    redirect_to explore_path
+  end
 end
